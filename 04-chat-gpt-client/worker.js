8df8836557efc95ebffe1a6a5d19237322e8ec9a
ignaccio7 <nacho260298@gmail.com>
2025-02-08 16:54:55 -0400

    Add fourth project

---
+++ 04-chat-gpt-client/worker.js
@@ -0,0 +1,8 @@
+// worker.ts
+import { WebWorkerMLCEngineHandler } from "https://esm.run/@mlc-ai/web-llm";
+
+// A handler that resides in the worker thread
+const handler = new WebWorkerMLCEngineHandler();
+self.onmessage = (msg) => {
+  handler.onmessage(msg);
+};
